a8e7bd411ef33f8a350b9276296f566e785cdfbb
Pedro Ulloa <pulloam@hotmail.com>
2022-11-30 21:50:00 -0300

    Agregar archivos de proyecto.

---
+++ PrimeraWeb/Controllers/ClientesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrimeraWeb.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Buscar(string nombre, string edad, string credito)
+        {
+            if(nombre != null) { 
+                return Content("<h1>Clientes -> " + nombre + "</h1> </hr> edad :" + edad + ", crédito : " + credito );
+            }else
+                return RedirectToAction("ListadoClientes");
+        }
+
+
+        public string ListadoClientes() {
+            string htmlCreado = "";
+            htmlCreado = "<ul>";
+            for (int i = 0;i < 10;i++) {
+                htmlCreado += "<li> Item " + i + "</li>";
+            }
+            htmlCreado = htmlCreado + "</ul>";
+            return htmlCreado;
+        }
+
+    }
+
+    
+
+}
